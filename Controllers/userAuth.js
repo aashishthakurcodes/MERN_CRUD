5fd5e2c25b5d0e9a599b93fd4fea94e65c9543d9
aashishthakurcodes <j9183208@gmail.com>
2024-01-03 07:15:13 -0800

    Routes updated

--- Controllers/userAuth.js
+++ Controllers/userAuth.js
@@ -1,5 +1,6 @@
 import userSchema from "../Modals/userSchema.js";
 import hashPassword from "../config/authHelper.js";
+import bcrypt from "bcrypt";
 
 
 export const registerUser = async (req, res) => {
@@ -22,7 +23,7 @@ export const registerUser = async (req, res) => {
     if (!password) {
       return res.send({ message: "Please fill your Password" });
     }
-    
+
     //Existing User
     const userExist = await userSchema.findOne({ email });
     if (userExist) {
@@ -33,26 +34,29 @@ export const registerUser = async (req, res) => {
     }
 
     //Hashing Password
-    const hashedPassword=await hashPassword(password)
+    const hashedPassword = await hashPassword(password);
 
     //Saving Data in DataBase
-    const user = new userSchema({ name, email, password:hashedPassword, phone, lname }).save();
+    const user = new userSchema({
+      name,
+      email,
+      password: hashedPassword,
+      phone,
+      lname,
+    }).save();
     //Sending Confirmation with user object
-    res.status(201).send(
-        {
-            success:true,
-            message: "User Register Successfully",
-            user: {
-              name: (await user).name,
-              email: (await user).email,
-              phone: (await user).phone,
-              address: (await user).lname,
-              password:(await user).password
-            },
-        }
-    );
+    res.status(201).send({
+      success: true,
+      message: "User Register Successfully",
+      user: {
+        name: (await user).name,
+        email: (await user).email,
+        phone: (await user).phone,
+        address: (await user).lname,
+        password: (await user).password,
+      },
+    });
   } catch (error) {
-    
     res.status(500).send({
       success: false,
       message: "Error in register",
@@ -60,3 +64,57 @@ export const registerUser = async (req, res) => {
     });
   }
 };
+
+
+// Login User
+// Login User
+export const loginUser = async (req, res) => {
+  try {
+
+    
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+
+    // Existing user
+    const user = await userSchema.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "E-mail is not registered",
+      });
+    }
+
+    // Check password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    // Send a successful response with user details
+    res.status(200).json({
+      success: true,
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.lname,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Error in login",
+      error: error.message,
+    });
+  }
+};
+
--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {registerUser} from '../Controllers/userAuth.js'
+import {loginUser, registerUser} from '../Controllers/userAuth.js'
 
 
 //Router Object
@@ -8,5 +8,6 @@ const router=express.Router();
 //routing
 //REGISTER || METHOD POST
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
